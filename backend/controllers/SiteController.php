3b89d3970089a8ac42b8d540db19d4054cec76d2
Bernardo <bernas.henriques.lopoes@gmail.com>
2025-11-04 17:23:05 +0000

    backend login

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -8,7 +8,6 @@ use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\Response;
-use backend\models\SignupForm;
 
 /**
  * Site controller
@@ -103,22 +102,5 @@ class SiteController extends Controller
         return $this->goHome();
     }
 
-    public function actionSignup()
-    {
-        $model = new SignupForm();
-        if ($model->load(Yii::$app->request->post()) && $model->signup()) {
-            Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
-            return $this->goHome();
-        }
-
-        return $this->render('signup', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Requests password reset.
-     *
-     * @return mixed
-     */
+    
 }

1e490d207abe85115d74046fe3b4f63c6fe4c76c
Guilherme Silva <guilhermemendessilva123@gmail.com>
2025-11-14 13:59:53 +0000

    rules da api no main.php e configuração da bd no main-local

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '6JCPlBaHkWOZwxy6MLSP--j5ig3dm5jf',
+            'cookieValidationKey' => 'oBMmWObeXA8Ifxr6VOPDQQwrSOBXwSGg',
         ],
     ],
 ];
--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'yDaUQR6N4eFq2Oxw9IYFOh31OCxE5hef',
+            'cookieValidationKey' => 'bQP0YxvO_e9CyuqLoCBSjZFz5JnBNh0s',
         ],
     ],
 ];
